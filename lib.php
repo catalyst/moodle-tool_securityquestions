98dba07141fcfc5a826b72e0d40bbffeba436f69
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-07-26 17:01:55 +1000

    Fixed XMLDB file

--- lib.php
+++ lib.php
@@ -27,11 +27,20 @@ defined('MOODLE_INTERNAL') || die;
 function tool_securityquestions_after_require_login() {
     global $USER;
     global $DB;
-    $requiredquestions = get_config('tool_securityquestions', 'minuserquestions');
-    $answeredquestions = $DB->get_records('tool_securityquestions_res', array('userid' => $USER->id));
-    $url = '/admin/tool/securityquestions/set_responses.php';
-    if (count($answeredquestions) < $requiredquestions) {
-        redirect($url);
+
+    //Check whether enough questions are set to make the plugin active
+    $setquestions = $DB->get_records('tool_securityquestions', array('deprecated' => 0));
+    $requiredset = get_config('tool_securityquestions', 'minquestions');
+
+    if (count($setquestions) >= $requiredset) {
+
+        // Check whether user has answered enough questions
+        $requiredquestions = get_config('tool_securityquestions', 'minuserquestions');
+        $answeredquestions = $DB->get_records('tool_securityquestions_res', array('userid' => $USER->id));
+        $url = '/admin/tool/securityquestions/set_responses.php';
+        if (count($answeredquestions) < $requiredquestions) {
+            redirect($url);
+        }
     }
 }
 
